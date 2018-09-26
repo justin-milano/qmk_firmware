fe22d32f8c07eca149ea59e7bbd0aa9fe84d9853
Justin Milano <jmilano@maverik.com>
2018-09-25 21:52:22 -0600

    adds doom... can this be overridden in rules.mk or something instead?

--- keyboards/planck/keymaps/jmilano/config.h
+++ keyboards/planck/keymaps/jmilano/config.h
@@ -4,7 +4,7 @@
 #include "config_common.h"
 
 #ifdef AUDIO_ENABLE
-    #define STARTUP_SONG SONG(PLANCK_SOUND)
+    #define STARTUP_SONG SONG(E1M1_DOOM)
     // #define STARTUP_SONG SONG(NO_SOUND)
 
     #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
