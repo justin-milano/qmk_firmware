17a82feca4c81b08a4a3dcad29c96d7f5396d7be
Justin Milano <jmilano@maverik.com>
2018-10-07 15:22:04 -0600

    tune mousekey timings and exclude RGB backlight

--- keyboards/chocopad/keymaps/jmilano/config.h
+++ keyboards/chocopad/keymaps/jmilano/config.h
@@ -5,11 +5,12 @@
 
 #define TAPPING_TOGGLE 2
 
-#define MOUSEKEY_DELAY             150
-#define MOUSEKEY_INTERVAL          50
-#define MOUSEKEY_MAX_SPEED         20
-#define MOUSEKEY_TIME_TO_MAX       20
-#define MOUSEKEY_WHEEL_MAX_SPEED   8
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 40
+// mousekey tune borowed from planck:pickle_jr
+#define MOUSEKEY_DELAY       0
+#define MOUSEKEY_INTERVAL    16
+#define MOUSEKEY_MAX_SPEED   7
+#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_WHEEL_DELAY 0
+
 
 #endif
