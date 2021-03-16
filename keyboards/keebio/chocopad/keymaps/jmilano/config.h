c486e18297b0dc10ab01599983ea26da3cf4d4dd
Justin Milano <jmilano@maverik.com>
2021-03-16 09:13:34 -0600

    pulls in personalized chocopad map from stale branch

---
+++ keyboards/keebio/chocopad/keymaps/jmilano/config.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#define TAPPING_TOGGLE 2
+
+// mousekey tune borowed from planck:pickle_jr
+#define MOUSEKEY_DELAY       0
+#define MOUSEKEY_INTERVAL    16
+#define MOUSEKEY_MAX_SPEED   7
+#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_WHEEL_DELAY 0
+
+
+#endif
